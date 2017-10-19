a0fad48082b89c91791c806be2c312a283b44645
leota <leotagit@gmail.com>
2017-10-19 10:58:46 +0200

    FIX: HttpService using Promises

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import 'rxjs/add/operator/toPromise';
 
 
 @Injectable()
@@ -9,7 +10,7 @@ export class HttpService {
 
   retreiveData(currency: string): Promise<any> {
     return new Promise((resolve, reject) => {
-      this.http.get('https://api.coinmarketcap.com/v1/ticker/' + currency + '/').subscribe(data => {
+      this.http.get('https://api.coinmarketcap.com/v1/ticker/' + currency + '/').toPromise().then(data => {
         return resolve(data);
       });
     })
